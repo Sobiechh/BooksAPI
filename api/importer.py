fec4bef70b36a67650590070c7a6dc4f74e99ddf
Piotr Sobieszczyk <p.sobieszczyk1@gmail.com>
2021-09-18 23:57:56 +0200

    Implement downloading data from api

---
+++ api/importer.py
@@ -0,0 +1,54 @@
+import requests
+
+from api.models import Book, Author, ISBN
+from api.parser import BooksParser
+
+
+class BooksImporter:
+    def __init__(self, url):
+        self.url = url
+        self.step = 40
+
+    def import_books(self):
+        raw_request: dict = self.get_url_request(self.url)
+
+        for start_index in self.get_start_index(raw_request):
+            url = self.url + f'&maxResults={self.step}&startIndex={start_index}'
+            request = self.get_url_request(url)
+
+            for book_data in request['items']:
+                book_info = BooksParser().book_parser(book_data)
+
+                book, created_book = Book.objects.get_or_create(
+                    book_id=book_info['book_id'],
+                    title=book_info['title'],
+                    published_date=book_info['published_date'],
+                    page_count=book_info['page_count'],
+                    book_cover_link=book_info['book_cover_link'],
+                    publication_language=book_info['publication_language']
+                )
+
+                for author_name in BooksParser().authors_parser(book_data):
+                    author, created_author = Author.objects.get_or_create(
+                        name=author_name
+                    )
+                    book.authors.add(author)
+
+                for industry in BooksParser().industry_parser(book_data):
+                    isbn, created_isbn = ISBN.objects.get_or_create(
+                        isbn_id=industry.get('identifier'),
+                        book=book
+                    )
+                    isbn.save()
+
+                book.save()
+
+    @staticmethod
+    def get_url_request(url: str) -> dict:
+        return requests.get(url=url).json()
+
+    def get_start_index(self, request):
+        items_count = request['totalItems']
+
+        for index in range(1, items_count, self.step - 1):
+            yield index - 1
--- api/management/commands/add_default_data.py
+++ api/management/commands/add_default_data.py
@@ -1,15 +1,12 @@
-import logging
 import requests
-from datetime import datetime
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
 from api.models import Book, Author, ISBN
-
-URL = "https://www.googleapis.com/books/v1/volumes?q=lion+intitle&maxResults=40"
+from api.importer import BooksImporter
 
 
 class Command(BaseCommand):
-    help = 'Add default books data'
+    help = 'Add some default books data- used only in development'
 
     def __init__(self, stdout=None, stderr=None, no_color=False, force_color=False):
         super().__init__(stdout, stderr, no_color, force_color)
@@ -29,29 +26,5 @@ class Command(BaseCommand):
 
             self.stdout.write(self.style.SUCCESS('All data cleaned'))
 
-        data: dict = requests.get(url=URL).json()
-
-        for book_data in data['items']:
-            id = book_data['id']
-            try:  # TODO data is sometimes in format yyyy-mm-dd
-                published_date = int(book_data['volumeInfo'].get('publishedDate', -1))
-            except:
-                published_date = -1
-            title = book_data['volumeInfo'].get('title', None)
-            page_count = int(book_data['volumeInfo'].get('pageCount', -1))
-            book_cover_link = book_data['volumeInfo']['imageLinks'].get('thumbnail', None)
-            publication_language = book_data['volumeInfo'].get('language', None)
-
-            book, created = Book.objects.get_or_create(
-                book_id=id,
-                title=title,
-                published_year=published_date,
-                page_count=page_count,
-                book_cover_link=book_cover_link,
-                publication_language=publication_language
-            )
-
-            if not created:
-                logging.log('Book exist')
-            else:
-                logging.info('Book created')
+        URL = "https://www.googleapis.com/books/v1/volumes?q=python+intitle"
+        BooksImporter(URL).import_books()
