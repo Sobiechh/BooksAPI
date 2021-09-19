1b7bdcd79b9db50ee071f1626788ade8da3cc023
Piotr Sobieszczyk <51410953+Sobiechh@users.noreply.github.com>
2021-09-20 01:15:00 +0200

    Add models crud

--- books/urls.py
+++ books/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('books', views.books_list, name='books'),
     path('', views.welcome, name='welcome'),
+    path('new', views.new, name='new'),
+    path('edit/<str:pk>', views.edit, name='edit'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from api.models import Book
 from books.filters import BookFilter
+from books.forms import BookForm
 
 
 def books_list(request):
@@ -16,6 +17,40 @@ def books_list(request):
     }
 
     return render(request, 'books_list/index.html', context)
+
+
+def new(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('books')
+    else:
+        form = BookForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'book_new/index.html', context)
+
+
+def edit(request, pk):
+    book = Book.objects.get(book_id=pk)
+
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('books')
+    else:
+        form = BookForm(instance=book)
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'book_new/index.html', context)
 
 
 def welcome(request):
