76c0f67ad5e5c51a62ad2d19ca27511809dda03c
Piotr Sobieszczyk <51410953+Sobiechh@users.noreply.github.com>
2021-09-19 23:57:27 +0200

    Create books list view + filter

--- books/views.py
+++ books/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 
 from api.models import Book
+from books.filters import BookFilter
 
 
 def books_list(request):
-    return render(request, "books_list.html",
-                  {"books": Book.objects.all()}
-                  )
+    books = Book.objects.all()
+
+    book_filter = BookFilter(request.GET, queryset=books)
+    books = book_filter.qs
+
+    context = {
+        'books': books,
+        'filter': book_filter,
+    }
+
+    return render(request, 'books_list/index.html', context)
 
 
 def welcome(request):
