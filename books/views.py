7b32391dd56f4fdd4073b36fc8e4e1c3adb51d69
Piotr Sobieszczyk <51410953+Sobiechh@users.noreply.github.com>
2021-09-19 19:06:07 +0200

    Configure basic urls

--- books/views.py
+++ books/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from api.models import Book
+
+
+def books_list(request):
+    return render(request, "books_list.html",
+                  {"books": Book.objects.all()}
+                  )
+
+
+def welcome(request):
+    return render(request, "welcome/index.html")
